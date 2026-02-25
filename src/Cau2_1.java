656ae2376b2ed23040519b887737d42501b43bf6
phuonglinhle268 <plinhle26@gmail.com>
2026-02-25 15:51:30 +0700

    hackathon

--- src/Cau2_1.java
+++ src/Cau2_1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Cau2_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
         int[] arr = new int[n];
 
--- src/Cau3.java
+++ src/Cau3.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Cau3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
     }
 }
